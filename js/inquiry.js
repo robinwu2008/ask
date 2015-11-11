fcaf8f7f62c0d7d856f8ebb4af242c552928ee64
robinwu2008 <robinwu2008@126.com>
2015-11-11 16:56:56 +0800

    xxx

--- js/inquiry.js
+++ js/inquiry.js
@@ -2,6 +2,15 @@
          var app = angular.module('inquiry',[]);
          app.controller('InquiryController',function($scope){
                    $scope.products = gem;
+				   
+				   $scope.data = {
+    availableOptions: [
+      {id: '1', name: 'MAX'},
+      {id: '2', name: 'Intel'},
+      {id: '3', name: 'Micro'}
+    ],
+    selectedOption: {id: '3', name: 'Option C'} //This sets the default value of the select in the ui
+    };
 			
           $scope.add=function(){	
 			     
@@ -19,7 +28,7 @@
          });
 		 
 		
-
+        
          
          var gem =[{
                    pn:'MAX232-1',
